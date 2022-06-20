332a7aca83ae47d8cdcaac0731e3a826a75cefc0
Jerome Dumonteil <jd@abilian.com>
2022-06-20 14:05:13 +0200

    nua_build: refactor commands for settings management, update Nua Readme

--- nua_build/tests/test_dump_load_settings.py
+++ nua_build/tests/test_dump_load_settings.py
@@ -1,18 +1,43 @@
+"""tests for 'nuad show' and 'nuad load'
+
+WIP to be continued...
+"""
 from typer.testing import CliRunner
 
 from nua_build.main import app
 
-runner = CliRunner()
-
 
 def test_dump_all_settings_0():
-    result = runner.invoke(app, "dump_all_settings")
+    runner = CliRunner()
+    result = runner.invoke(app, "settings show --all")
 
     assert result.exit_code == 0
 
 
-def test_dump_nua_settings_0():
-    result = runner.invoke(app, "dump_nua_settings")
+def test_dump_nua_settings_1():
+    runner = CliRunner()
+    result = runner.invoke(app, "settings show")
+
+    assert result.exit_code == 0
+
+
+def test_dump_nua_settings_2():
+    runner = CliRunner()
+    result = runner.invoke(app, "settings show nua")
+
+    assert result.exit_code == 0
+
+
+def test_dump_nua_settings_3():
+    runner = CliRunner()
+    result = runner.invoke(app, "settings show nua --json")
+
+    assert result.exit_code == 0
+
+
+def test_dump_nua_settings_3():
+    runner = CliRunner()
+    result = runner.invoke(app, "settings show nua --toml")
 
     assert result.exit_code == 0
 
