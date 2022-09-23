2a4e6c961c3a783c0cf7b5011161aa8b4455c031
Jerome Dumonteil <jd@abilian.com>
2022-09-23 08:51:26 +0200

    Certbot option "none"

--- nua_orchestrator_local/nua_orchestrator_local/certbot.py
+++ nua_orchestrator_local/nua_orchestrator_local/certbot.py
@@ -1,24 +1,29 @@
-"""Certbot rules."""
+"""Certbot main entry, detect strategy and apply.
+
+host.certbot_strategy (or ENV NUA_CERTBOT_STRATEGY) can be (for now):
+    - auto: all domains are declared to certbot to use HTTPS
+    - none: no HTTPS domain, all is converted to HTTP only (ie. tests or local server)
+    - Default is "auto"
+
+Test ENV variables:
+    - NUA_CERTBOT_VERBOSE: show certbot log
+    - NUA_CERTBOT_TEST: use certbot test environment (pnly for tests)
+"""
 
 import os
-import sys
 
 from . import config
-from .rich_console import print_magenta, print_red
-from .shell import sh
-from .state import verbosity
+from .certbot_strategies import apply_auto_strategy, apply_none_strategy
+from .rich_console import panic, print_red
 
-
-def assert_certbot_strategy():
-    certbot_strategy = config.read("nua", "host", "certbot_strategy")
-    if certbot_strategy != "auto":
-        print_red("Error: only certbot_strategy='auto' is implemented. Exiting.")
-        sys.exit(1)
+ALLOWED_STRATEGY = {"auto": apply_auto_strategy, "none": apply_none_strategy}
 
 
 def register_certbot_domains(filtered_sites: list):
-    """Group common domains and execute "certbot run".
+    """Apply certbot strategy to domains.
 
+    Group common domains and execute "certbot run".
+    (Only public function).
     Warning:
        - Top domain "exemple.com" is NOT configurd for cerbot if only
          "www.exemple.com" is listed,
@@ -33,61 +38,23 @@ def register_certbot_domains(filtered_sites: list):
         domains = domain_dict.get(top_domain, set())
         domains.add(site_domain)
         domain_dict[top_domain] = domains
+    strategy = fetch_certbot_strategy()
+    apply_function = ALLOWED_STRATEGY[strategy]
     for domains in domain_dict.values():
-        certbot_run_https(list(domains))
+        apply_function(list(domains))
 
 
-def certbot_run_args(domains: list) -> str:
-    run_args = [
-        "--nginx",
-        "--keep",
-        "--renew-with-new-domains",
-        "--allow-subset-of-names",
-        "--expand",
-        "--agree-tos",
-        "-n",
-        "--rsa-key-size 4096",
-        # "--key-type rsa", for newer certbot versions
-        "--redirect",
-    ]
-    if not os.environ.get("NUA_CERTBOT_VERBOSE"):
-        run_args.append("--quiet")
-    if os.environ.get("NUA_CERTBOT_TEST"):
-        run_args.append("--test-cert")
-    admin_mail = config.read("nua", "host", "host_admin_mail")
-    if admin_mail:
-        email = f"--email {admin_mail}"
-    else:
-        email = "--register-unsafely-without-email"
-    run_args.append(email)
-    dom_list = " ".join(f"-d {d}" for d in domains)
-    run_args.append(dom_list)
-    return " ".join(run_args)
+def fetch_certbot_strategy() -> str:
+    default = "auto"
+    strategy = config.read("nua", "host", "certbot_strategy") or default
+    strategy = os.environ.get("NUA_CERTBOT_STRATEGY") or strategy
+    strategy = strategy.strip().lower()
+    assert_valid_certbot_strategy(strategy)
+    return strategy
 
 
-def certbot_run_https(domains: list[str]):
-    """Convert just created http configuration to https using certbot.
-
-    Each domain of the list uses the same ssl key.
-
-    - implementation mode "auto":
-        - let certbot rewrite redirections
-        - let certbot manage cron
-        - apply "auto" rules and parameters
-    """
-    assert_certbot_strategy()
-    if verbosity(1):
-        print_magenta(f"Run Certbot for: {' '.join(domains)}")
-    cmd = "certbot run " + certbot_run_args(domains)
-    if os.getuid():  # aka not root
-        cmd = "sudo " + cmd
-    # Important:
-    # - command is executed as root
-    # - when "sudo cmd", from nua, we epect to use the host's certbot package
-    #   of the host installation (not a venv package)
-    # - certbot has been installed on the host from nua-bootstrap
-    if verbosity(3):
-        output = sh(cmd, show_cmd=True, capture_output=True)
-        print(output)
-    else:
-        sh(cmd, show_cmd=verbosity(2))
+def assert_valid_certbot_strategy(strategy: str):
+    if strategy not in ALLOWED_STRATEGY:
+        print_red("Allowed values for certbot_strategy are:")
+        print_red(f"{ALLOWED_STRATEGY}")
+        panic(f"Unknown certbot_strategy: {strategy}")
---
+++ nua_orchestrator_local/nua_orchestrator_local/certbot_strategies.py
@@ -0,0 +1,83 @@
+"""Certbot strategies.
+
+host.certbot_strategy (or ENV NUA_CERTBOT_STRATEGY) can be (for now):
+    - auto: all domains are declared to certbot to use HTTPS
+    - none: no HTTPS domain, all is converted to HTTP only (ie. tests or local server)
+    - Default is "auto"
+
+Test ENV variables:
+    - NUA_CERTBOT_VERBOSE: show certbot log
+    - NUA_CERTBOT_TEST: use certbot test environment (pnly for tests)
+"""
+
+import os
+
+from . import config
+from .rich_console import print_magenta
+from .shell import sh
+from .state import verbosity
+
+
+def certbot_run_args(domains: list) -> str:
+    """Build cerbot's arguments for a domain and subdomains.
+
+    (Local function)"""
+    run_args = [
+        "--nginx",
+        "--keep",
+        "--renew-with-new-domains",
+        "--allow-subset-of-names",
+        "--expand",
+        "--agree-tos",
+        "-n",
+        "--rsa-key-size 4096",
+        # "--key-type rsa", for newer certbot versions
+        "--redirect",
+    ]
+    if not os.environ.get("NUA_CERTBOT_VERBOSE"):
+        run_args.append("--quiet")
+    if os.environ.get("NUA_CERTBOT_TEST"):
+        run_args.append("--test-cert")
+    admin_mail = config.read("nua", "host", "host_admin_mail")
+    if admin_mail:
+        email = f"--email {admin_mail}"
+    else:
+        email = "--register-unsafely-without-email"
+    run_args.append(email)
+    dom_list = " ".join(f"-d {d}" for d in domains)
+    run_args.append(dom_list)
+    return " ".join(run_args)
+
+
+def apply_none_strategy(domains: list[str]):
+    if verbosity(1):
+        print_magenta(f"Use HTTP protocol for: {' '.join(domains)}")
+    return
+
+
+def apply_auto_strategy(domains: list[str]):
+    """Convert just created HTTP configuration (by nginx template) to HTTPS using
+    certbot (if strategy is 'auto').
+
+    Each domain of the list uses the same SSL key.
+
+    - implementation mode "auto":
+        - let certbot rewrite redirections,
+        - let certbot manage cron,
+        - apply "auto" rules and parameters.
+    """
+    if verbosity(1):
+        print_magenta(f"Use HTTPS protocol (run Certbot) for: {' '.join(domains)}")
+    cmd = "certbot run " + certbot_run_args(domains)
+    if os.getuid():  # aka not root
+        cmd = "sudo " + cmd
+    # Important:
+    # - command is executed as root
+    # - when "sudo cmd", from nua, we epect to use the host's certbot package
+    #   of the host installation (not a venv package)
+    # - certbot has been installed on the host from nua-bootstrap
+    if verbosity(3):
+        output = sh(cmd, show_cmd=True, capture_output=True)
+        print(output)
+    else:
+        sh(cmd, show_cmd=verbosity(2))
