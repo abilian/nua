04a04f5610f57c01de330e1b31e9769450b22bb2
Stefane Fermigier <sf@abilian.com>
2022-11-11 20:01:55 +0100

    refact: clean up tests a bit.

--- nua_build/tests/test_apache_basic.py
+++ nua_build/tests/test_apache_basic.py
@@ -1,3 +1,8 @@
+"""To be preferably launched with :
+
+pytest -rP test_apache_basic.py
+"""
+
 import shlex
 import socket
 import subprocess as sp  # noqa, required.
@@ -25,153 +30,145 @@ def check_port_available(host: str, port: str | int, timeout: int = 1) -> bool:
     return True
 
 
-class TestApacheBasic:
-    def test_complete_build_without_cache(self):
-        """To be preferably launched with :
+def test_complete_build_without_cache():
+    host_port = 8081
 
-        pytest -rP test_apache_basic.py
-        """
-        host_port = 8081
+    assert check_port_available("127.0.0.1", host_port)
 
-        assert check_port_available("127.0.0.1", host_port)
+    src_path = Path(__file__).parent / "apps" / "apache_basic"
+    image_target = "nua-apache-basic:2.4.52-2"
+    with tempfile.TemporaryDirectory(dir="/tmp") as tmpdirname:
+        build_dir = Path(tmpdirname) / "build"
+        copytree(src_path, build_dir)
+        dock = docker.from_env()
 
-        src_path = Path(__file__).parent / "apps" / "apache_basic"
-        image_target = "nua-apache-basic:2.4.52-2"
-        with tempfile.TemporaryDirectory(dir="/tmp") as tmpdirname:
-            build_dir = Path(tmpdirname) / "build"
-            copytree(src_path, build_dir)
-            dock = docker.from_env()
-
-            for image in (
-                NUA_LINUX_BASE,
-                NUA_PYTHON_TAG,
-                NUA_BUILDER_TAG,
-                image_target,
-            ):
-                print(f"Show '{image}' in cache:", dock.images.list(image))
-
-            print("Clean cache.")
-            dock.images.prune()
-
-            for image in reversed(
-                (NUA_LINUX_BASE, NUA_PYTHON_TAG, NUA_BUILDER_TAG, image_target)
-            ):
-                with suppress(docker.errors.ImageNotFound):
-                    dock.images.remove(image, force=True, noprune=False)
-                assert not dock.images.list(image)
-
-            cmd = shlex.split("nua-build --version")
-            result = sp.run(cmd, capture_output=True)
-            print(result.stdout.decode("utf8"))
-
-            # DTZ005 The use of `datetime.datetime.now()` without `tz` argument is
-            # not allowed : well, by default it uses localtime.
-            print("Time now:", datetime.now(timezone.utc).isoformat(" "))
-            print(f"Build {image_target} (no images in cache) with command:")
-            cmd = shlex.split(f"nua-build {build_dir}")
-            print(f"'{cmd}'")
-            t0 = perf_counter()
-            result = sp.run(cmd, capture_output=True)
-            print("Time now:", datetime.now(timezone.utc).isoformat(" "))
-            print("elapsed (s):", perf_counter() - t0)
-            print(" ========= result.stdout ===========")
-            print(result.stdout.decode("utf8"))
-            print(result.stderr.decode("utf8"))
-            print(" ===================================")
-            assert result.returncode == 0
-            assert dock.images.list(image_target)
-
-        print("Testing the container:")
-        # clean previous run if any
-        for previous in dock.containers.list(filters={"ancestor": image_target}):
-            previous.kill()
-        container = dock.containers.run(
+        for image in (
+            NUA_LINUX_BASE,
+            NUA_PYTHON_TAG,
+            NUA_BUILDER_TAG,
             image_target,
-            detach=True,
-            ports={"80/tcp": host_port},
-        )
-        sleep(2)
-        cmd = shlex.split(f"curl http://127.0.0.1:{host_port}")
-        # S603 subprocess call - check for execution of untrusted input.: wrong
-        curl_result = sp.run(cmd, capture_output=True)
-        stdout = curl_result.stdout.decode("utf8")
-        stderr = curl_result.stderr.decode("utf8")
-        print("stdout:\n", stdout)
-        print("stderr:\n", stderr)
+        ):
+            print(f"Show '{image}' in cache:", dock.images.list(image))
 
-        assert curl_result.returncode == 0
+        print("Clean cache.")
+        dock.images.prune()
 
-        test_string = f"<h1>Nua test {nua_version}</h1>"
+        for image in reversed(
+            (NUA_LINUX_BASE, NUA_PYTHON_TAG, NUA_BUILDER_TAG, image_target)
+        ):
+            with suppress(docker.errors.ImageNotFound):
+                dock.images.remove(image, force=True, noprune=False)
+            assert not dock.images.list(image)
 
-        assert test_string in stdout
+        cmd = shlex.split("nua-build --version")
+        result = sp.run(cmd, capture_output=True)
+        print(result.stdout.decode("utf8"))
 
-        container.kill()
+        # DTZ005 The use of `datetime.datetime.now()` without `tz` argument is
+        # not allowed : well, by default it uses localtime.
+        print("Time now:", datetime.now(timezone.utc).isoformat(" "))
+        print(f"Build {image_target} (no images in cache) with command:")
+        cmd = shlex.split(f"nua-build {build_dir}")
+        print(f"'{cmd}'")
+        t0 = perf_counter()
+        result = sp.run(cmd, capture_output=True)
+        print("Time now:", datetime.now(timezone.utc).isoformat(" "))
+        print("elapsed (s):", perf_counter() - t0)
+        print(" ========= result.stdout ===========")
+        print(result.stdout.decode("utf8"))
+        print(result.stderr.decode("utf8"))
+        print(" ===================================")
+        assert result.returncode == 0
+        assert dock.images.list(image_target)
 
-    def test_complete_build_with_cache(self):
-        """To be preferably launched with :
+    print("Testing the container:")
+    # clean previous run if any
+    for previous in dock.containers.list(filters={"ancestor": image_target}):
+        previous.kill()
+    container = dock.containers.run(
+        image_target,
+        detach=True,
+        ports={"80/tcp": host_port},
+    )
+    sleep(2)
+    cmd = shlex.split(f"curl http://127.0.0.1:{host_port}")
+    # S603 subprocess call - check for execution of untrusted input.: wrong
+    curl_result = sp.run(cmd, capture_output=True)
+    stdout = curl_result.stdout.decode("utf8")
+    stderr = curl_result.stderr.decode("utf8")
+    print("stdout:\n", stdout)
+    print("stderr:\n", stderr)
 
-        pytest -rP test_apache_basic.py
-        """
-        host_port = 8081
+    assert curl_result.returncode == 0
 
-        assert check_port_available("127.0.0.1", host_port)
+    test_string = f"<h1>Nua test {nua_version}</h1>"
 
-        src_path = Path(__file__).parent / "apps" / "apache_basic"
-        image_target = "nua-apache-basic:2.4.52-2"
-        with tempfile.TemporaryDirectory(dir="/tmp") as tmpdirname:
-            build_dir = Path(tmpdirname) / "build"
-            copytree(src_path, build_dir)
-            dock = docker.from_env()
+    assert test_string in stdout
 
-            for image in (
-                NUA_LINUX_BASE,
-                NUA_PYTHON_TAG,
-                NUA_BUILDER_TAG,
-                image_target,
-            ):
-                print(f"Show '{image}' in cache:", dock.images.list(image))
+    container.kill()
 
-            cmd = shlex.split("nua-build --version")
-            result = sp.run(cmd, capture_output=True)
-            print(result.stdout.decode("utf8"))
 
-            print("Time now:", datetime.now(timezone.utc).isoformat(" "))
-            print(f"Build {image_target} (no images in cache) with command:")
-            cmd = shlex.split(f"nua-build {build_dir}")
-            print(f"'{cmd}'")
-            t0 = perf_counter()
-            result = sp.run(cmd, capture_output=True)
-            print("Time now:", datetime.now(timezone.utc).isoformat(" "))
-            print("elapsed (s):", perf_counter() - t0)
-            print(" ========= result.stdout ===========")
-            print(result.stdout.decode("utf8"))
-            print(result.stderr.decode("utf8"))
-            print(" ===================================")
-            assert result.returncode == 0
-            assert dock.images.list(image_target)
+def test_complete_build_with_cache():
+    host_port = 8081
 
-        print("Testing the container:")
-        # clean previous run if any
-        for previous in dock.containers.list(filters={"ancestor": image_target}):
-            previous.kill()
-        container = dock.containers.run(
+    assert check_port_available("127.0.0.1", host_port)
+
+    src_path = Path(__file__).parent / "apps" / "apache_basic"
+    image_target = "nua-apache-basic:2.4.52-2"
+    with tempfile.TemporaryDirectory(dir="/tmp") as tmpdirname:
+        build_dir = Path(tmpdirname) / "build"
+        copytree(src_path, build_dir)
+        dock = docker.from_env()
+
+        for image in (
+            NUA_LINUX_BASE,
+            NUA_PYTHON_TAG,
+            NUA_BUILDER_TAG,
             image_target,
-            detach=True,
-            ports={"80/tcp": host_port},
-        )
-        sleep(2)
-        cmd = shlex.split(f"curl http://127.0.0.1:{host_port}")
-        # S603 subprocess call - check for execution of untrusted input.: wrong
-        curl_result = sp.run(cmd, capture_output=True)
-        stdout = curl_result.stdout.decode("utf8")
-        stderr = curl_result.stderr.decode("utf8")
-        print("stdout:\n", stdout)
-        print("stderr:\n", stderr)
+        ):
+            print(f"Show '{image}' in cache:", dock.images.list(image))
 
-        assert curl_result.returncode == 0
+        cmd = shlex.split("nua-build --version")
+        result = sp.run(cmd, capture_output=True)
+        print(result.stdout.decode("utf8"))
 
-        test_string = f"<h1>Nua test {nua_version}</h1>"
+        print("Time now:", datetime.now(timezone.utc).isoformat(" "))
+        print(f"Build {image_target} (no images in cache) with command:")
+        cmd = shlex.split(f"nua-build {build_dir}")
+        print(f"'{cmd}'")
+        t0 = perf_counter()
+        result = sp.run(cmd, capture_output=True)
+        print("Time now:", datetime.now(timezone.utc).isoformat(" "))
+        print("elapsed (s):", perf_counter() - t0)
+        print(" ========= result.stdout ===========")
+        print(result.stdout.decode("utf8"))
+        print(result.stderr.decode("utf8"))
+        print(" ===================================")
+        assert result.returncode == 0
+        assert dock.images.list(image_target)
 
-        assert test_string in stdout
+    print("Testing the container:")
+    # clean previous run if any
+    for previous in dock.containers.list(filters={"ancestor": image_target}):
+        previous.kill()
+    container = dock.containers.run(
+        image_target,
+        detach=True,
+        ports={"80/tcp": host_port},
+    )
+    sleep(2)
+    cmd = shlex.split(f"curl http://127.0.0.1:{host_port}")
+    # S603 subprocess call - check for execution of untrusted input.: wrong
+    curl_result = sp.run(cmd, capture_output=True)
+    stdout = curl_result.stdout.decode("utf8")
+    stderr = curl_result.stderr.decode("utf8")
+    print("stdout:\n", stdout)
+    print("stderr:\n", stderr)
 
-        container.kill()
+    assert curl_result.returncode == 0
+
+    test_string = f"<h1>Nua test {nua_version}</h1>"
+
+    assert test_string in stdout
+
+    container.kill()
--- nua_build/tests/test_builds.py
+++ nua_build/tests/test_builds.py
@@ -6,7 +6,7 @@ from .build_image import build_test_image
 
 root = Path(__file__).parent
 
-app_folder = [
+app_dirs = [
     "echo_two_ports",
     "flask_one_poetry",
     "flask_one_setup",
@@ -24,8 +24,10 @@ app_folder = [
     "flask_mariadb_wheel",
 ]
 
+# app_dirs = [dir for dir in (root / "apps").iterdir() if dir.is_dir()]
 
-@pytest.mark.parametrize("app", app_folder)
-def test_build_all(app):
-    build_test_image(root / "apps" / app)
-    print(Path.cwd())
+
+@pytest.mark.parametrize("dir_name", app_dirs)
+def test_build_all(dir_name: str):
+    path = root / "apps" / dir_name
+    build_test_image(path)
