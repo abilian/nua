17181453f247cf21082007bf28c9eb1899ddbd68
Stefane Fermigier <sf@abilian.com>
2022-11-11 12:46:55 +0100

    chore: forgot noxfile.

---
+++ nua_orchestrator/noxfile.py
@@ -0,0 +1,19 @@
+import nox
+
+PYTHON_VERSIONS = ["3.10", "3.11"]
+
+nox.options.reuse_existing_virtualenvs = True
+nox.options.sessions = ("pytest", "lint")
+
+
+@nox.session(python="python3.10")
+def lint(session: nox.Session) -> None:
+    session.run("poetry", "install", "-q", external=True)
+    session.run("make", "lint", external=True)
+
+
+@nox.session(python=PYTHON_VERSIONS)
+def pytest(session: nox.Session) -> None:
+    session.run("poetry", "install", "-q", external=True)
+    session.run("pip", "check")
+    session.run("pytest", "-q")
