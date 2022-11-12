a38a4280572fd4c83e6c166e906370d19d65714a
Stefane Fermigier <sf@abilian.com>
2022-11-12 23:15:29 +0100

    ci: tweak noxfile.

--- nua_orchestrator/noxfile.py
+++ nua_orchestrator/noxfile.py
@@ -3,17 +3,16 @@ import nox
 PYTHON_VERSIONS = ["3.10", "3.11"]
 
 nox.options.reuse_existing_virtualenvs = True
-nox.options.sessions = ("pytest", "lint")
 
 
 @nox.session(python="python3.10")
 def lint(session: nox.Session) -> None:
-    session.run("poetry", "install", "-q", external=True)
+    session.run("poetry", "install", external=True)
     session.run("make", "lint", external=True)
 
 
 @nox.session(python=PYTHON_VERSIONS)
 def pytest(session: nox.Session) -> None:
-    session.run("poetry", "install", "-q", external=True)
+    session.run("poetry", "install", external=True)
     session.run("pip", "check")
-    session.run("pytest", "-q")
+    session.run("pytest")
