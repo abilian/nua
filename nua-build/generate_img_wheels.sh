77d4d5a0085e35268a7f62c111380b4a18ce3f0b
Jerome Dumonteil <jd@abilian.com>
2022-11-16 15:31:17 +0100

    add script to generate some wheels before doing nua-selfimage package

---
+++ nua-build/generate_img_wheels.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+#  Todo: make this part of a nua-build python script (or nua-selfimage)
+#
+
+echo "Make wheels for Nua base image if required:"
+
+version=$(nua-build --version |sed 's/.*:\s*\(.*\)$/\1/')
+WHEEL_DIR="/var/tmp/nua_build_wheel_${version}"
+
+rm -fr "${WHEEL_DIR}"
+mkdir -p "${WHEEL_DIR}"
+
+cd ../nua-lib
+rm -fr dist
+poetry build -f wheel
+mv dist/* "${WHEEL_DIR}"
+
+cd ../nua-runtime
+rm -fr dist
+poetry build -f wheel
+mv dist/* "${WHEEL_DIR}"
+
+cd ../nua-build
+rm -fr dist
+poetry build -f wheel
+mv dist/* "${WHEEL_DIR}"
