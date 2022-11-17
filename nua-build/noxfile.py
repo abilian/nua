78889a4518ad01e19532462f594061cf6dc14ca2
Stefane Fermigier <sf@abilian.com>
2022-11-17 18:55:52 +0100

    chore: faster nox sessions.

--- nua-build/noxfile.py
+++ nua-build/noxfile.py
@@ -3,24 +3,24 @@ import nox
 # PYTHON_VERSIONS = ["3.10", "3.11"]
 PYTHON_VERSIONS = ["3.10"]
 
+nox.options.reuse_existing_virtualenvs = True
 
 @nox.session(python=PYTHON_VERSIONS, venv_backend="venv")
 def lint(session: nox.Session) -> None:
-    with session.chdir("../nua-lib"):
-        session.run("poetry", "install", external=True)
-    with session.chdir("../nua-runtime"):
-        session.run("poetry", "install", external=True)
-    session.run("poetry", "install", external=True)
+    _install(session)
     session.run("pip", "check", external=True)
     session.run("make", "lint", external=True)
 
 
 @nox.session(python=PYTHON_VERSIONS, venv_backend="venv")
 def pytest(session: nox.Session) -> None:
+    session.run("bash", "generate_img_wheels.sh", external=True)
+    session.run("pytest", "--tb=short", external=True)
+
+
+def _install(session):
+    session.run("poetry", "install", "--sync", external=True)
     with session.chdir("../nua-lib"):
         session.run("poetry", "install", external=True)
     with session.chdir("../nua-runtime"):
         session.run("poetry", "install", external=True)
-    session.run("poetry", "install", external=True)
-    session.run("bash", "generate_img_wheels.sh", external=True)
-    session.run("pytest", "--tb=short", external=True)
