a14bb21d4d9df27254a7fe79ef7e16c284c66ee4
Stefane Fermigier <sf@abilian.com>
2022-11-23 22:52:01 +0100

    chore: better top-level noxfile.

--- noxfile.py
+++ noxfile.py
@@ -1,20 +1,18 @@
-# import time
-# from random import randint
-
 import nox
 from nox.sessions import Session
+
 
 BASE_PYTHON_VERSION = "3.10"
 PYTHON_VERSIONS = ["3.10"]
 
 
-nox.options.pythons = ["3.10"]
+nox.options.pythons = PYTHON_VERSIONS
 nox.options.default_venv_backend = "venv"
-nox.options.sessions = [
-    "lint",
-    "pytest",
-    "doc",
-]
+# nox.options.sessions = [
+#     "lint",
+#     "pytest",
+#     "doc",
+# ]
 
 SUB_REPOS = [
     "nua-lib",
@@ -26,29 +24,31 @@ SUB_REPOS = [
 
 
 @nox.session(python=PYTHON_VERSIONS)
-def lint(session: nox.Session):
-    run_subsessions(session)
+@nox.parametrize("sub_repo", SUB_REPOS)
+def pytest(session: nox.Session, sub_repo: str):
+    run_subsession(session, sub_repo)
 
 
-@nox.session(python=PYTHON_VERSIONS)
-def pytest(session: nox.Session):
-    run_subsessions(session)
+@nox.session(python=BASE_PYTHON_VERSION)
+@nox.parametrize("sub_repo", SUB_REPOS)
+def lint(session: nox.Session, sub_repo: str):
+    run_subsession(session, sub_repo)
 
 
-@nox.session(python=PYTHON_VERSIONS)
+@nox.session(python=BASE_PYTHON_VERSION)
 def doc(session: nox.Session):
     print("TODO: do something with the docs")
 
 
-def run_subsessions(session: nox.Session):
-    for sub_repo in SUB_REPOS:
-        print(f"\nRunning session: {session.name} in subrepo: {sub_repo}\n")
-        with session.chdir(sub_repo):
-            session.run(
-                "nox",
-                "-e",
-                session.name,
-                "--reuse-existing-virtualenvs",
-                external=True,
-            )
-        print()
+def run_subsession(session, sub_repo):
+    name = session.name.split("(")[0]
+    print(f"\nRunning session: {session.name} in subrepo: {sub_repo}\n")
+    with session.chdir(sub_repo):
+        session.run(
+            "nox",
+            "-e",
+            name,
+            "--reuse-existing-virtualenvs",
+            external=True,
+        )
+    print()
