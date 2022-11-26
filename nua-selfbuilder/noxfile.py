7cc949a4c445dc4a433bd5e7f1ada9e4385631c3
Stefane Fermigier <sf@abilian.com>
2022-11-26 17:55:53 +0100

    chore: same as previous.

--- nua-selfbuilder/noxfile.py
+++ nua-selfbuilder/noxfile.py
@@ -6,21 +6,18 @@ PYTHON_VERSIONS = ["3.10"]
 
 @nox.session(python=PYTHON_VERSIONS, venv_backend="venv")
 def lint(session: nox.Session) -> None:
-    with session.chdir("../nua-lib"):
-        session.run("poetry", "install", external=True)
-    with session.chdir("../nua-runtime"):
-        session.run("poetry", "install", external=True)
-    session.run("poetry", "install", external=True)
-    session.run("pip", "check", external=True)
+    install(session)
     session.run("make", "lint", external=True)
 
 
 @nox.session(python=PYTHON_VERSIONS, venv_backend="venv")
 def pytest(session: nox.Session) -> None:
-    with session.chdir("../nua-lib"):
-        session.run("poetry", "install", external=True)
-    with session.chdir("../nua-runtime"):
-        session.run("poetry", "install", external=True)
+    install(session)
+    session.run("pytest", "--tb=short", external=True)
+
+
+def install(session: nox.Session):
+    session.run("pip", "install", "../nua-lib", "../nua-runtime")
     session.run("poetry", "install", external=True)
     session.run("pip", "check", external=True)
-    session.run("pytest", "--tb=short", external=True)
+    session.run("poetry", "check", external=True)
