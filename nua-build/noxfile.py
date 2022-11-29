d63883da7b2807934fb249fe3f7b917484a8eb68
Stefane Fermigier <sf@abilian.com>
2022-11-29 09:13:49 +0100

    chore: simplify noxfile

--- nua-build/noxfile.py
+++ nua-build/noxfile.py
@@ -6,13 +6,13 @@ PYTHON_VERSIONS = ["3.10"]
 nox.options.reuse_existing_virtualenvs = True
 
 
-@nox.session(python=PYTHON_VERSIONS, venv_backend="venv")
+@nox.session
 def lint(session: nox.Session) -> None:
     _install(session)
     session.run("make", "lint", external=True)
 
 
-@nox.session(python=PYTHON_VERSIONS, venv_backend="venv")
+@nox.session(python=PYTHON_VERSIONS)
 def pytest(session: nox.Session) -> None:
     _install(session)
     session.run("pytest", "--tb=short", external=True)
