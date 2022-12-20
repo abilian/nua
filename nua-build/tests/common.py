fa9cb8b0a1b2dd6caa9922a86cf7ccf3a4b6d5b5
Stefane Fermigier <sf@abilian.com>
2022-12-20 22:32:24 +0100

    fix type error.

--- nua-build/tests/common.py
+++ nua-build/tests/common.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
 
-def get_apps_root_dir(subdir: str = None) -> Path:
+def get_apps_root_dir(subdir: str = "") -> Path:
     """Return the path to the root of the apps directory."""
     path = Path(__file__).parent.parent.parent / "apps"
-    if subdir is None:
+    if subdir == "":
         return path
     else:
         return path / subdir
