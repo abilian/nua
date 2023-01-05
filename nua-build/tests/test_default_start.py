fbce90dfc04f4537148f794a71f169901126b71b
Stefane Fermigier <sf@abilian.com>
2023-01-05 15:21:01 +0100

    fix test + coverage.

--- nua-build/tests/test_default_start.py
+++ nua-build/tests/test_default_start.py
@@ -7,6 +7,6 @@ def test_default_start():  # noqa
     f = io.StringIO()
     with redirect_stdout(f), suppress(ValueError):
         # Value Error if user Nua not created in buid environment (which is ok) :
-        runpy.run_module("nua.build.defaults.start.py")
+        runpy.run_module("nua.build.defaults.start")
     output = f.getvalue()
     assert "This start.py file only" in output
