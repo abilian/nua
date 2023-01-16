2ce32060e518181e727d257e877a12754b9e0335
Stefane Fermigier <sf@abilian.com>
2023-01-16 19:25:35 +0100

    Add docstrings / short help message for the Invoke tasks.

--- tasks.py
+++ tasks.py
@@ -13,10 +13,15 @@ SUB_REPOS = [
 #
 # Tasks
 #
+@task
+def install(c):
+    """Install all sub-packages (and dependencies)"""
+    run_in_subrepos(c, "pip install -e .")
 
 
 @task
 def lint(c):
+    """Lint (static check) the whole project."""
     # c.run("ruff .")
     # c.run("pre-commit run --all-files")
 
@@ -25,41 +30,49 @@ def lint(c):
 
 @task
 def format(c):  # noqa: A001
+    """Format the whole project."""
     run_in_subrepos(c, "make format")
 
 
 @task
 def test(c):
+    """Run tests (in each subrepo)."""
     run_in_subrepos(c, "make test")
 
 
 @task
 def mypy(c):
+    """Run mypy (in each subrepo)."""
     run_in_subrepos(c, "mypy src")
 
 
 @task
 def pyright(c):
+    """Run pyright (in each subrepo)."""
     run_in_subrepos(c, "pyright src")
 
 
 @task
 def clean(c):
+    """Clean the whole project."""
     run_in_subrepos(c, "make clean")
 
 
 @task
 def fix(c):
+    """Run ruff fixes in all subrepos."""
     run_in_subrepos(c, "ruff --fix src tests")
 
 
 @task
 def update(c):
+    """Update dependencies the whole project."""
     run_in_subrepos(c, "poetry update && poetry install")
 
 
 @task
 def graph(c):
+    """Generate dependency graph in all subprojects."""
     run_in_subrepos(c, "mkdir -p doc")
     for sub_repo in SUB_REPOS:
         output = "doc/dependency-graph.png"
