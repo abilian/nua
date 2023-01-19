a74b903299a444f68040572e8c478dcbd2964671
Jerome Dumonteil <jd@abilian.com>
2023-01-19 15:52:23 +0100

    remove some useless test

--- apps/sample-apps/flask_sqla_sqlite_bind_wheel/flask_sqla_sqlite_bind/_test.sh
+++
@@ -1,3 +0,0 @@
-export FLASK_APP=app
-flask run
-# see: http://127.0.0.1:5000/
--- apps/sample-apps/flask_sqla_sqlite_bind_wheel/flask_sqla_sqlite_bind/constants.py
+++
@@ -1,4 +0,0 @@
-import os
-
-DB_FOLDER = os.environ.get("DB_FOLDER") or "/nua/app/dbs"
-DB_NAME = os.environ.get("DB_NAME") or "default_test.db"
