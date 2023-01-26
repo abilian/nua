9ca096f7db0a6b75bd3d9e970484c8519a34f653
Jerome Dumonteil <jd@abilian.com>
2023-01-26 14:05:59 +0100

    upgrade sample app "hedgedoc"

--- apps/real-apps/hedgedoc/nua/start.py
+++ apps/real-apps/hedgedoc/nua/start.py
@@ -2,25 +2,4 @@ import os
 
 from nua.lib.exec import exec_as_nua
 
-# Nua shortcuts to manage postgres operations
-from nua.runtime.db.postgres_manager import PostgresManager
-
-
-def setup_db():
-    """Find or create the required DB for app user.
-
-    In this example The DB is on remote docker container.
-    """
-    manager = PostgresManager(
-        host=os.environ.get("CMD_DB_HOST"), port=os.environ.get("CMD_DB_PORT")
-    )
-    manager.wait_for_db()
-    manager.setup_db_user(
-        os.environ.get("CMD_DB_DATABASE"),
-        os.environ.get("CMD_DB_USERNAME"),
-        os.environ.get("CMD_DB_PASSWORD"),
-    )
-
-
-setup_db()
 exec_as_nua("node app.js", cwd="/nua/build/hedgedoc", env=os.environ)
