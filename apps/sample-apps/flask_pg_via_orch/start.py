b5c72a3aaf3bac4c186a3634ec4e4c980e5c7aa9
Jerome Dumonteil <jd@abilian.com>
2023-01-26 14:18:11 +0100

    add sample app "flask_pg_via_orch"

--- apps/sample-apps/flask_pg_via_orch/start.py
+++ apps/sample-apps/flask_pg_via_orch/start.py
@@ -5,12 +5,14 @@ https://www.digitalocean.com/community/tutorials/how-to-use-a-postgresql-databas
 
 import os
 
-from flask_pg_test.init_db import init_db
+from flask_pg_via_orch.init_db import init_db
 from nua.lib.exec import exec_as_nua
 
 init_db()
 
-cmd = "gunicorn --worker-tmp-dir /dev/shm --workers 2 -b :5000 flask_pg_test.wsgi:app"
+cmd = (
+    "gunicorn --worker-tmp-dir /dev/shm --workers 2 -b :5000 flask_pg_via_orch.wsgi:app"
+)
 # exec_as_nua(cmd, env)
 # We need to exec as root to be able to write files in the docker volume.
 exec_as_nua(cmd, env=os.environ)
