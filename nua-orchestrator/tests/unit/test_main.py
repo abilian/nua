c79bf8b28af830b45d80e827be1a8276bd42e543
Stefane Fermigier <sf@abilian.com>
2023-02-08 22:59:11 +0100

    refact: test (introduce fixture).

--- nua-orchestrator/tests/unit/test_main.py
+++ nua-orchestrator/tests/unit/test_main.py
@@ -1,7 +1,13 @@
+from pytest import fixture
 from typer.testing import CliRunner
 
 from nua.orchestrator import __version__
 from nua.orchestrator.cli.main import app
+
+
+@fixture()
+def runner():
+    return CliRunner()
 
 
 def test_version_string():  # noqa
@@ -11,53 +17,36 @@ def test_version_string():  # noqa
     assert len(version_split) >= 2
 
 
-def test_version():
-    runner = CliRunner()
-
+def test_version(runner: CliRunner):
     result = runner.invoke(app, "--version")
-
     assert result.exit_code == 0
     assert "Nua orchestrator local" in result.stdout
     assert __version__ in result.stdout
 
 
-def test_version_short():
-    runner = CliRunner()
-
+def test_version_short(runner: CliRunner):
     result = runner.invoke(app, "-V")
-
     assert result.exit_code == 0
     assert "Nua orchestrator local" in result.stdout
     assert __version__ in result.stdout
 
 
-def test_bad_arg():
-    runner = CliRunner()
-
+def test_bad_arg(runner: CliRunner):
     result = runner.invoke(app, "bad_arg")
-
     assert result.exit_code == 2
 
 
-def test_verbose():
-    runner = CliRunner()
-
+def test_verbose(runner: CliRunner):
     result = runner.invoke(app, "--verbose")
-
     assert result.exit_code == 2
-
-
-def test_verbose_short():
-    runner = CliRunner()
 
     result = runner.invoke(app, "-v")
-
     assert result.exit_code == 2
 
 
-def test_rpc():
-    runner = CliRunner()
-
-    result = runner.invoke(app, "rpc", "ping")
-
-    assert result.exit_code == 0
+# def test_rpc():
+#     runner = CliRunner()
+#
+#     result = runner.invoke(app, "rpc", "ping")
+#
+#     assert result.exit_code == 0
