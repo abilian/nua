68ee0f74845b7781e79b243b25d0f1f461ffb12f
Stefane Fermigier <sf@abilian.com>
2023-02-17 17:00:38 +0100

    doc: forgot to add symlinks.

--- nua-cli/src/nua_cli/main.py
+++ nua-cli/src/nua_cli/main.py
@@ -83,19 +83,40 @@ def backup():
 # TODO: application lifecycle operations
 #
 @app.command()
-def build(path: str = "."):
+def build(
+    path: str = ".",
+    experimental: bool = typer.Option(
+        False, "--experimental", "-x", help="Use experimental builder (from nua-dev)."
+    ),
+    verbose: int = typer.Option(
+        0, "--verbose", "-v", help="Increase verbosity level, until -vv. ", count=True
+    ),
+    quiet: int = typer.Option(
+        0, "--quiet", "-q", help="Decrease verbosity level", count=True
+    ),
+):
     """Build app but don't deploy it."""
-    subprocess.run(["nua-dev", "build", path])
+
+    verbosity = 1 + verbose - quiet
+    verbosity_flags = verbosity * ["-v"]
+
+    if experimental:
+        typer.secho(
+            "Using experimental builder (from nua-dev).", fg=typer.colors.YELLOW
+        )
+        subprocess.run(["nua-dev", "build", path])
+    else:
+        subprocess.run(["nua-build"] + verbosity_flags + [path])
 
 
 @app.command()
-def deploy(imagename: str, domainname: str):
+def deploy(image: str, domain: str):
     """Deploy an application."""
     deply_config = {
         "site": [
             {
-                "domain": domainname,
-                "image": imagename,
+                "domain": domain,
+                "image": image,
             },
         ],
     }
