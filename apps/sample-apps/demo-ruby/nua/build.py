ad01c09690e8e6e6d035f347d5ed7ed5af57007d
Jerome Dumonteil <jd@abilian.com>
2023-02-24 15:21:56 +0100

    fix demo-ruby app

--- apps/sample-apps/demo-ruby/nua/build.py
+++ apps/sample-apps/demo-ruby/nua/build.py
@@ -2,24 +2,24 @@ from nua.agent.nua_config import NuaConfig
 from nua.lib.actions import install_nodejs
 from nua.lib.backports import chdir
 from nua.lib.exec import exec_as_nua
-from nua.lib.shell import chown_r, sh
+from nua.lib.shell import chown_r
 
 
 def main():
     config = NuaConfig()
     install_nodejs()
     src = config.fetch_source()
-    print(src)
     chown_r(src, "nua")
-    print("chown done")
-    sh("ls -l")
     with chdir(src):
-        sh("pwd")
-        sh("ls -l")
-        exec_as_nua("cp .env.example .env")
-        exec_as_nua("bundle install")
-        exec_as_nua("yarn install")
-        exec_as_nua("bundle exec rake db:setup")
+        exec_as_nua(
+            [
+                "cp .env.example .env",
+                "bundle install",
+                "bundle add tzinfo-data",
+                "yarn install",
+                "bundle exec rake db:setup",
+            ]
+        )
 
 
 if __name__ == "__main__":
