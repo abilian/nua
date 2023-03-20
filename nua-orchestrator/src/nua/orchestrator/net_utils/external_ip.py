797ac3a9fc877dc88348cd3dd3dfba2bea35fc22
Jerome Dumonteil <jd@abilian.com>
2023-03-20 16:18:39 +0100

    add external_ip() utility

---
+++ nua-orchestrator/src/nua/orchestrator/net_utils/external_ip.py
@@ -0,0 +1,14 @@
+from urllib.request import urlopen
+
+
+def external_ip() -> str:
+    """Detect the host external IPv4 address, by calling server icanhazip.com."""
+    try:
+        return (
+            urlopen("https://ipv4.icanhazip.com", timeout=30)  # noqa s310
+            .read()
+            .strip()
+            .decode("utf8")
+        )
+    except OSError:
+        return ""
