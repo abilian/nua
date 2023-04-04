5327cb5a6213764344cf532b9db8523a10a31c5a
Stefane Fermigier <sf@fermigier.com>
2023-04-04 23:30:03 +0200

    fix: script was broken when called with no arg.

--- nua-orchestrator/src/nua/orchestrator/scripts/print_instances.py
+++ nua-orchestrator/src/nua/orchestrator/scripts/print_instances.py
@@ -9,13 +9,15 @@ from ..nua_db_setup import setup_nua_db
 def main():
     if len(sys.argv) > 1:
         filter = sys.argv[1]
+    else:
+        filter = None
     setup_nua_db()
     instances = store.list_instances_all()
     for instance in instances:
         content = instance.to_dict()
         if filter and content["site_config"]["instance_name"] != filter:
             continue
-        pprint(instance.to_dict())
+        pprint(content)
 
 
 if __name__ == "__main__":
