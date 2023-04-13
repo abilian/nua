ec2a23b3bbbd5417c07ef1d1fea482bec9279645
Stefane Fermigier <sf@fermigier.com>
2023-04-13 09:55:01 +0200

    feat: add `nua env` commands.

---
+++ nua-cli/src/nua_cli/commands/env.py
@@ -0,0 +1,36 @@
+from operator import itemgetter
+from pprint import pp
+
+from nua_cli.base import Argument, Command
+from nua_cli.client import get_client
+from nua_cli.common import get_current_app_id
+from nua_cli.exceptions import BadArgument
+
+client = get_client()
+
+
+class EnvShowCommand(Command):
+    """Show application env variables."""
+
+    name = "env show"
+
+    arguments = [
+        Argument("app_id", nargs='?', help="Application ID"),
+    ]
+
+    def run(self, app_id: str = ""):
+        # Quick & dirty implementation that calls docker directly.
+        if not app_id:
+            app_id = get_current_app_id()
+        app_info = client.get_app_info(app_id)
+        for k, v in app_info["site_config"]["env"].items():
+            print(f"{k}={repr(v)}")
+
+
+class EnvCommand(Command):
+    """Manage an app's env variables."""
+
+    name = "env"
+
+    def run(self):
+        self.cli.print_help()
