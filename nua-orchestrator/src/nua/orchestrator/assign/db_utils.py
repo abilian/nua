1d9adb1484413ab82335b28cc71df7a713250382
Stefane Fermigier <sf@fermigier.com>
2023-04-16 10:40:21 +0200

    fix typing error.

--- nua-orchestrator/src/nua/orchestrator/assign/db_utils.py
+++ nua-orchestrator/src/nua/orchestrator/assign/db_utils.py
@@ -4,11 +4,11 @@ from ..db import store
 from ..utils import base20
 
 
-def generate_new_user_id() -> int:
+def generate_new_user_id() -> str:
     number = store.new_user_number()
     return f"user_{base20(number)}"
 
 
-def generate_new_db_id() -> int:
+def generate_new_db_id() -> str:
     number = store.new_user_number()
     return f"db_{base20(number)}"
