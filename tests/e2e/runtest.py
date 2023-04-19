ec23f7211a70eb9733657d2bbcfeff75602f99b2
Stefane Fermigier <sf@fermigier.com>
2023-04-19 16:46:32 +0200

    test: e2e tests (WIP: testing build).

--- tests/e2e/runtest.py
+++ tests/e2e/runtest.py
@@ -17,14 +17,22 @@ BOXES = {
 
 
 def main():
+    sh("mkdir -p apps")
+    sh("cp -a ../../apps/real-apps/ apps/")
+
     generate_vagrantfile()
-
-    if "running" in sh("vagrant status", capture=True):
-        print("Vagrant is already running")
-    else:
-        sh("vagrant up --provider qemu")
-
+    vagrant_up()
     ssh("curl -L https://nua.rocks/install.py | sudo python3")
+
+    ssh("sudo cp -r .ssh /home/nua/")
+    ssh("sudo chown -R nua.nua /home/nua/.ssh")
+
+    for app_dir in Path("apps").iterdir():
+        app_name = app_dir.name
+        ssh(
+            f"cd /vagrant/apps/{app_name} && /home/nua/env/bin/nua-build -vv",
+            user="nua",
+        )
 
 
 def generate_vagrantfile():
@@ -33,6 +41,20 @@ def generate_vagrantfile():
     Path("Vagrantfile").write_text(
         Path("Vagrantfile.tpl").read_text().replace("##BOXNAME##", boxname)
     )
+
+
+def vagrant_up():
+    if "running" in sh("vagrant status", capture=True):
+        print("Vagrant is already running")
+        return
+
+    if platform.system() == "Darwin":
+        sh("vagrant up --provider qemu")
+    else:
+        sh("vagrant up")
+
+    ssh_config = sh("vagrant ssh-config", capture=True)
+    Path("ssh-config").write_text(ssh_config)
 
 
 def sh(cmd: str, cwd: str = ".", capture=False) -> str:
@@ -55,11 +77,15 @@ def sh(cmd: str, cwd: str = ".", capture=False) -> str:
         print(f"Command failed: {e.cmd}")
 
 
-def ssh(cmd: str):
+def ssh(cmd: str, user="vagrant"):
     """Run a ssh command."""
-    print(dim(f'Running "{cmd}" on server...'))
+    if user == "vagrant":
+        print(dim(f'Running "{cmd}" on vagrant...'))
+    else:
+        print(dim(f'Running "{cmd}" on vagrant as "{user}"...'))
+
     # args = shlex.split(cmd)
-    cmd = ["vagrant", "ssh", "-c", cmd]  # type: ignore
+    cmd = ["ssh", "-F", "ssh-config", f"{user}@default", cmd]  # type: ignore
     try:
         subprocess.run(cmd, check=True)
     except subprocess.CalledProcessError as e:
