6e83c7cf735cc1bd898537662cc04dbbb44d0e78
Jerome Dumonteil <jd@abilian.com>
2023-05-29 15:41:49 +0000

    feat(chore): update, bump version

--- nua-lib/src/nua/lib/actions/__init__.py
+++ nua-lib/src/nua/lib/actions/__init__.py
@@ -21,12 +21,12 @@ from .ruby import install_ruby
 from .util import (
     camel_format,
     copy_from_package,
-    to_kebab_cases,
-    to_snake_cases,
     download_extract,
     replace_in,
     snake_format,
     string_in,
+    to_kebab_cases,
+    to_snake_cases,
 )
 
 __all__ = [
