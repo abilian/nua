8fa8484a4311fef689c17f9e3972d467da9b55ee
Jerome Dumonteil <jd@abilian.com>
2023-06-05 11:26:23 +0200

    fix(lib):remove unused types.py

--- nua-lib/src/nua/lib/types.py
+++
@@ -1,7 +0,0 @@
-# TODO: duplicated
-
-from typing import TypeAlias
-
-JSON: TypeAlias = dict[str, "JSON"] | list["JSON"] | str | int | float | bool | None
-JsonDict: TypeAlias = dict[str, JSON]
-JsonList: TypeAlias = list[JSON]
