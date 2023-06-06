8a9c09c075c1071be8ae9637b0a6e6087a089b43
Jerome Dumonteil <jd@abilian.com>
2023-06-06 16:08:27 +0200

    feat(orch): add backup plugin for mongodb

--- nua-orchestrator/src/nua/orchestrator/backup/plugins/mongodump.py
+++ nua-orchestrator/src/nua/orchestrator/backup/plugins/mongodump.py
@@ -1,4 +1,4 @@
-"""Class to backup a database."""
+"""Class to backup a MongoDB database."""
 
 
 from ...docker_utils import docker_container_of_name, docker_exec_checked
@@ -6,13 +6,13 @@ from ..backup_registry import register_plugin
 from .plugin_base_class import BackupErrorException, PluginBaseClass
 
 
-class BckPostgresDumpall(PluginBaseClass):
-    """Backup plugin for Postgres Docker continaer using pg_dumpall.
+class BckMongodump(PluginBaseClass):
+    """Backup plugin for MongoDB Docker container using mongodump.
 
-    To be used at Resource level, on a Postgres container.
+    To be used at Resource level, on a MongoDB container.
     """
 
-    identifier = "pg_dumpall"
+    identifier = "mongodump"
 
     def check_local_destination(self) -> None:
         backup_destination = self.options.get("destination", "local")
@@ -21,20 +21,23 @@ class BckPostgresDumpall(PluginBaseClass):
         raise BackupErrorException("WIP: Only local backup is currently implemented")
 
     def do_backup(self) -> None:
-        """Backup the Resource with pg_dumpall.
+        """Backup the Resource with mongodump.
 
-        Resource is expected to be a Postgres database.
+        Resource is expected to be a mongodump database.
         """
         self.check_local_destination()
 
-        self.file_name = f"{self.date}-{self.node}.sql"
+        self.file_name = f"{self.date}-{self.node}.archive"
         dest_file = self.folder / self.file_name
 
         container = docker_container_of_name(self.node)
         if container is None:
             raise BackupErrorException(f"Error: No container found for {self.node}")
 
-        cmd = "/usr/bin/pg_dumpall -U ${POSTGRES_USER}"
+        cmd = (
+            "/usr/bin/mongodump -u ${MONGO_INITDB_ROOT_USERNAME} "
+            "-p ${MONGO_INITDB_ROOT_PASSWORD} --archive"
+        )
 
         print(f"Start backup: {dest_file}")
         with dest_file.open("wb") as output:
@@ -47,4 +50,4 @@ class BckPostgresDumpall(PluginBaseClass):
         self.finalize()
 
 
-register_plugin(BckPostgresDumpall)
+register_plugin(BckMongodump)
