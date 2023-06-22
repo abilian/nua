80b70164928335c0a68de96a08ae28fb497668f5
Jerome Dumonteil <jd@abilian.com>
2023-06-22 10:06:51 +0200

    fix(apps): fix demo-flask

--- apps/sample-apps/demo-flask/start.py
+++
@@ -1,7 +0,0 @@
-from nua.lib.exec import exec_as_root
-
-cmd = "gunicorn --worker-tmp-dir /dev/shm --workers 1 -b :80 app:app"
-
-# exec_as_nua(cmd, env)
-# We need to exec as root to be able to write files in the docker volume.
-exec_as_root(cmd)
