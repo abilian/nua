a977690654067430e13edda282f57c5843bca291
Jerome Dumonteil <jd@abilian.com>
2023-09-05 15:08:50 +0200

    feat(build): update Ubuntu image to ubuntu:jammy-20230816

--- nua-build/src/nua/build/autobuild/constants.py
+++ nua-build/src/nua/build/autobuild/constants.py
@@ -3,7 +3,7 @@ from ..version import __version__
 CODE_URL = "https://github.com/abilian/nua/archive/refs/heads/main.zip"
 
 # Ubuntu 22.04.1 "jammy" LTS with python3.10
-NUA_LINUX_BASE = "ubuntu:jammy-20230425"
+NUA_LINUX_BASE = "ubuntu:jammy-20230816"
 
 NUA_PYTHON_TAG = f"nua-python:{__version__}"
 
